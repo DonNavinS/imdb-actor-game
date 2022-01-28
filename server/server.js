d5b9430bdb017beddd93a0ba256ddbc5d52fbfc0
DonNavinS <seneviratnanavin@gmail.com>
2022-01-28 13:10:01 -0500

    installed tailwind css

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
