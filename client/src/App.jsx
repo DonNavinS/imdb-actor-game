2a03ae11dc4f235fd4a10d14609a7c8a254b2e6c
DonNavinS <seneviratnanavin@gmail.com>
2022-01-28 19:05:12 -0500

    added two imdb APIs to return ID and cast members

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,24 +2,48 @@ import { useState } from "react";
 import "./App.css";
 import Axios from "axios";
 function App() {
+  const [movieName, setMovieName] = useState("");
+  const [movieResults, setMovieResults] = useState([]);
   const [actors, setActors] = useState([]);
 
-  const getData = () => {
+  const getCast = () => {
     var options = {
       method: "GET",
-      url: "https://imdb8.p.rapidapi.com/title/get-full-credits",
-      params: { tconst: "tt0944947" },
+      url: "https://data-imdb1.p.rapidapi.com/movie/id/tt0086250/cast/",
       headers: {
-        "x-rapidapi-host": "imdb8.p.rapidapi.com",
+        "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
         "x-rapidapi-key": "7bca20d701msh6e19f6d29e75ea8p1fda7bjsnb742d201c4e9",
       },
     };
 
     Axios.request(options)
       .then(function (response) {
-        const firstTen = response.data.cast.slice(0, 10);
-        console.log(firstTen);
-        setActors(firstTen);
+        setActors(
+          response.data.results.roles.filter(
+            (item) => item.role !== "Director" && item.role !== "Writer"
+          )
+        );
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const getMovieID = () => {
+    var options = {
+      method: "GET",
+      url: "https://movie-database-imdb-alternative.p.rapidapi.com/",
+      params: { s: movieName, r: "json", page: "1" },
+      headers: {
+        "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+        "x-rapidapi-key": "7bca20d701msh6e19f6d29e75ea8p1fda7bjsnb742d201c4e9",
+      },
+    };
+
+    Axios.request(options)
+      .then(function (response) {
+        console.log(response.data.Search);
+        setMovieResults(response.data.Search);
       })
       .catch(function (error) {
         console.error(error);
@@ -28,10 +52,15 @@ function App() {
 
   return (
     <div>
-      <button onClick={getData}>get</button>
+      <button onClick={getMovieID}>get</button>
+      <input
+        type="text"
+        placeholder="Search Movie Name"
+        onChange={(e) => setMovieName(e.target.value)}
+      />
       <div>
-        {actors.map((item, index) => {
-          return <p key={index}>{item.name}</p>;
+        {movieResults.map((item, index) => {
+          return <div key={index}>{item.Title}</div>;
         })}
       </div>
     </div>
