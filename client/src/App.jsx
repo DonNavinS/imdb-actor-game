e735fc700284d52c53574ff269c8ad657eaae7fd
DonNavinS <seneviratnanavin@gmail.com>
2022-01-28 21:08:58 -0500

    movie search functioning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -51,16 +51,39 @@ function App() {
   };
 
   return (
-    <div>
-      <button onClick={getMovieID}>get</button>
-      <input
-        type="text"
-        placeholder="Search Movie Name"
-        onChange={(e) => setMovieName(e.target.value)}
-      />
-      <div>
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-gradient-to-tr from-violet-400 to-green-400 ">
+      <div className="flex gap-x-4 -translate-y-1/2">
+        <button
+          className="px-2 py-1 rounded border-double border-2 border-red-600 hover:text-white hover:bg-red-600 hover:scale-110 tranformation duration-200"
+          onClick={getMovieID}
+        >
+          Search Movies
+        </button>
+        <input
+          className="rounded p-1"
+          type="text"
+          placeholder="Search Movie Name"
+          onChange={(e) => setMovieName(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-wrap justify-center items-baseline ">
         {movieResults.map((item, index) => {
-          return <div key={index}>{item.Title}</div>;
+          return (
+            <div
+              key={index}
+              className="flex flex-col basis-1/5 justify-center items-center"
+            >
+              <button>
+                <img
+                  width="150vw"
+                  height="150vh"
+                  src={item.Poster}
+                  alt={`${item.Title} movie poster `}
+                />
+              </button>
+              <p className="text-center">{item.Title}</p>
+            </div>
+          );
         })}
       </div>
     </div>
