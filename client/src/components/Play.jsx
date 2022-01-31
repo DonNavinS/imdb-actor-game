5731ea43cd239f6837f1ed07c04f15f8d545d509
DonNavinS <seneviratnanavin@gmail.com>
2022-01-31 00:44:35 -0500

    second round now works when guessing movie names

--- client/src/components/Play.jsx
+++ client/src/components/Play.jsx
@@ -3,20 +3,34 @@ import { useGlobalState } from "../GlobalState";
 import Axios from "axios";
 
 function Play() {
-  const [actors, setActors] = useGlobalState("actors");
-  const [actorID, setActorID] = useState("");
   const [selectedMovie, setSelectedMovie] = useGlobalState("selectedMovie");
+  const [actors, setActors] = useGlobalState("actors");
   const [guess, setGuess] = useGlobalState("guess");
   const [guessResult, setGuessResult] = useState(null);
+  const [actorID, setActorID] = useState("");
   const [movies, setMovies] = useState([]);
+  const [actorImage, setActorImage] = useState("");
+  const [actorName, setActorName] = useState("");
+  const [movieRound, setMovieRound] = useState(true);
 
   const actorsNames = actors.map((item) => item.name);
+  const movieNames = movies;
 
-  const submitGuess = () => {
+  const submitActorGuess = () => {
     if (actorsNames.includes(guess)) {
       const newGuessResult = actorsNames.includes(guess);
       setGuessResult(newGuessResult);
       setActorID(actors.filter((item) => item.name === guess)[0].id);
+    } else {
+      setGuessResult(false);
+    }
+  };
+
+  const submitMovieGuess = () => {
+    if (movieNames.includes(guess)) {
+      setGuessResult(true);
+    } else {
+      setGuessResult(false);
     }
   };
 
@@ -24,8 +38,9 @@ function Play() {
     setGuessResult(null);
     setGuess("");
     setActorID("");
+    setMovieRound(false);
 
-    var options = {
+    var optionsForID = {
       method: "GET",
       url: `https://data-imdb1.p.rapidapi.com/actor/id/${actorID}/all_roles/`,
       params: { page_size: "50" },
@@ -35,7 +50,7 @@ function Play() {
       },
     };
 
-    Axios.request(options)
+    Axios.request(optionsForID)
       .then(function (response) {
         console.log(response.data.results.map((item) => item[0].title));
         setMovies(response.data.results.map((item) => item[0].title));
@@ -43,17 +58,48 @@ function Play() {
       .catch(function (error) {
         console.error(error);
       });
+
+    var optionsForActorInfo = {
+      method: "GET",
+      url: `https://data-imdb1.p.rapidapi.com/actor/id/${actorID}/`,
+      headers: {
+        "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
+        "x-rapidapi-key": "7bca20d701msh6e19f6d29e75ea8p1fda7bjsnb742d201c4e9",
+      },
+    };
+
+    Axios.request(optionsForActorInfo)
+      .then(function (response) {
+        setActorImage(response.data.results.image_url);
+        setActorName(response.data.results.name);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   };
-  console.log(movies);
   return (
     <div className="flex justify-center items-center">
       <div className="flex flex-col items-center">
-        <img
-          className="w-48 h-64 rounded-lg"
-          src={selectedMovie.image}
-          alt={`${selectedMovie.title} poster`}
-        />
-        <p className="text-center">{selectedMovie.title}</p>
+        {movieRound ? (
+          <div>
+            <img
+              className="w-48 h-64 rounded-lg"
+              src={selectedMovie.image}
+              alt={`${selectedMovie.title} poster`}
+            />
+            <p className="text-center">{selectedMovie.title}</p>
+          </div>
+        ) : (
+          <div>
+            <img
+              className="w-48 h-64 rounded-lg"
+              src={actorImage}
+              alt={`${actorName} poster`}
+            />
+            <p className="text-center">{selectedMovie.title}</p>
+          </div>
+        )}
+
         <input
           className="border-2 border-black rounded p-1"
           placeholder="Enter Actor Name"
@@ -78,7 +124,11 @@ function Play() {
             </div>
           )
         ) : null}
-        <button onClick={submitGuess}>Guess</button>
+        {movieRound ? (
+          <button onClick={submitActorGuess}>Guess</button>
+        ) : (
+          <button onClick={submitMovieGuess}>Guess</button>
+        )}
       </div>
     </div>
   );
